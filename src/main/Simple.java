4343c89a148f07f8134db0c88e208f31ae137ea2
SeoJiHyeong <21000353@handong.edu>
2016-12-02 14:16:58 +0900

    toJacoco

---
+++ src/main/Simple.java
@@ -0,0 +1,11 @@
+public class Simple 
+{
+	public Simple() {
+	}
+
+	public int exec(int i) {
+		if (i > 0)
+			return i ;
+		return i * -1 ;
+	}
+}
---
+++ src/test/SimpleTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test ;
+import static org.junit.Assert.* ;
+
+public class SimpleTest
+{
+	@Test
+	public void testSimple1() {
+		Simple s = new Simple() ;
+		assertTrue(s.exec(-1) == 1) ;
+	}
+
+	@Test
+	public void testSimple2() {
+		Simple s = new Simple() ;
+		assertTrue(s.exec(1) == 1) ;
+	}
+
+	public static void main(String [] args) {
+		SimpleTest s = new SimpleTest() ;
+
+		s.testSimple1() ;
+		s.testSimple2() ;
+	}
+}
