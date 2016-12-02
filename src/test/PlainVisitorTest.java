19c53ff89fa3ee7b6592a521c9c052064a8fe38e
SeoJiHyeong <21000353@handong.edu>
2016-12-02 21:59:56 +0900

    j

---
+++ src/test/PlainVisitorTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test ;
+import static org.junit.Assert.* ;
+
+public class PlainVisitorTest
+{
+    
+	@Test
+	public void testvisit() {
+        PlainVisitor v = PlainVisitor();
+		assertTrue(s.exec(-1) == 1) ;
+	}
+
+	
+
+}
