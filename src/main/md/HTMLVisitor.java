082c59a57754eeb0cf63663db104c02f4c828aa9
hbs1215 <hbs1215@naver.com>
2016-12-07 01:00:03 +0900

    add check notice

--- src/main/md/HTMLVisitor.java
+++ src/main/md/HTMLVisitor.java
@@ -13,7 +13,7 @@ public class HTMLVisitor implements MDElementVisitor {
 	private Node tempNode;
  	//node
 	private ArrayList<Node> nodeList = new ArrayList<Node>();
-	
+
 
 	//it will save converted htmlcode
 	private ArrayList<String> HTMLList = new ArrayList();
@@ -26,37 +26,59 @@ public class HTMLVisitor implements MDElementVisitor {
 
 	 //node
 	 public void visit(Header n){
-		tempNode = nodeList.get(sequence);
-		ArrayList<Token> tokenList = tempNode.getTokenList();
-		System.out.println(tempNode.notice);
-		System.out.println(tempNode.htype);
-		System.out.println(tokenList.get(0).notice);
-		System.out.println(tokenList.get(0).content);
-		
+		if(n.notifyNode().equals=="Header"){
+			tempNode = nodeList.get(sequence);
+			ArrayList<Token> tokenList = tempNode.getTokenList();
+			System.out.println(tempNode.notice);
+			System.out.println(tempNode.htype);
+			System.out.println(tokenList.get(0).notice);
+			System.out.println(tokenList.get(0).content);
+		}
+		else;
+
+
 	 }
 
 	 public void visit(ItemList n){
+		if(n.notifyNode().equals=="ItemList"){
 
+		}
+		else;
 	 }
 
 	 public void visit(OrderedList n){
+		if(n.notifyNode().equals=="OrderedList"){
 
+		}
+		else;
 	 }
 
 	 public void visit(HorizontalRule n){
+		if(n.notifyNode().equals=="HorizontalRule"){
 
+		}
+		else;
 	 }
 
 	 public void visit(Text n){
+		if(n.notifyNode().equals=="Text"){
 
+		}
+		else;
 	 }
 
 	 public void visit(BlockQuotes n){
+		if(n.notifyNode().equals=="BlockQuotes"){
 
+		}
+		else;
 	 }
 
 	 public void visit(CodeBlock n){
+		if(n.notifyNode().equals=="CodeBlock"){
 
+		}
+		else;
 	 }
 
 
