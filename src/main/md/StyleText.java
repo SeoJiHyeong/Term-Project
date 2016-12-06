195f126540bb05be1bd4ba36d29cad850adfdf8d
hbs1215 <hbs1215@naver.com>
2016-12-07 06:14:58 +0900

    Add files via upload

--- src/main/md/StyleText.java
+++ src/main/md/StyleText.java
@@ -1,24 +1,25 @@
 package md;
+
 public class StyleText extends Token{
 	String content;
 	public StyleText(String s){
 		switch(s) {
 		case "em":
-			notice = "em";
+			notice = "<em>";
 			break;
 		case "/em":
-			notice = "/em";
+			notice = "</em>";
 			break;
 		case "strong":
-			notice = "strong";
+			notice = "<strong>";
 			break;
 		case "/strong":
-			notice = "/strong";
+			notice = "</strong>";    //add '< >' on all components
 			break;
 		}
 	}
 
-	
+
 	public void accept(MDElementVisitor v) {
 		v.visit(this);
 	}
