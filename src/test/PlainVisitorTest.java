b518a87a8573608f91f2a3c67b5943568276a425
SeoJiHyeong <21000353@handong.edu>
2016-12-07 04:55:34 +0900

    change

--- src/test/PlainVisitorTest.java
+++ src/test/PlainVisitorTest.java
@@ -16,108 +16,74 @@ public class PlainVisitorTest
         nodeList = p.getNode();
         tokenList = nod.getTokenList();
         assertEquals("test",tokenList.get(1).getContent());
-	}
-
-    @Test
-    public void testEmphasis_0() {
-        PlainVisitor p = new PlainVisitor();
-        String tmp = "__test";
-        Text nod = new Text();
-        ArrayList<Node> nodeList = new ArrayList<Node>();
-        ArrayList<Token> tokenList = new ArrayList<Token>();
+        p = new PlainVisitor();
+         tmp = "__test";
+        nod = new Text();
+        nodeList = new ArrayList<Node>();
+        tokenList = new ArrayList<Token>();
         p.tokenize(tmp,nod);
         nodeList = p.getNode();
         tokenList = nod.getTokenList();
         assertEquals("__test",tokenList.get(0).getContent());
-    }
-
-
-    @Test
-    public void testEmphasis_2() {
-        PlainVisitor p = new PlainVisitor();
-        String tmp = "**test**";
-        Text nod = new Text();
-        ArrayList<Node> nodeList = new ArrayList<Node>();
-        ArrayList<Token> tokenList = new ArrayList<Token>();
+         p = new PlainVisitor();
+         tmp = "**test**";
+         nod = new Text();
+         nodeList = new ArrayList<Node>();
+         tokenList = new ArrayList<Token>();
         p.tokenize(tmp,nod);
         nodeList = p.getNode();
         tokenList = nod.getTokenList();
         assertEquals("test",tokenList.get(1).getContent());
-    }
-    
-    @Test
-    public void testEmphasis_3() {
-        PlainVisitor p = new PlainVisitor();
-        String tmp = "__test_h";
-        Text nod = new Text();
-        ArrayList<Node> nodeList = new ArrayList<Node>();
-        ArrayList<Token> tokenList = new ArrayList<Token>();
+         p = new PlainVisitor();
+         tmp = "__test_h";
+         nod = new Text();
+         nodeList = new ArrayList<Node>();
+         tokenList = new ArrayList<Token>();
         p.tokenize(tmp,nod);
         nodeList = p.getNode();
         tokenList = nod.getTokenList();
         assertEquals("test",tokenList.get(2).getContent());
-    }
-    
-    @Test
-    public void testEmphasis_4() {
-        PlainVisitor p = new PlainVisitor();
-        String tmp = "**test*h";
-        Text nod = new Text();
-        ArrayList<Node> nodeList = new ArrayList<Node>();
-        ArrayList<Token> tokenList = new ArrayList<Token>();
+        p = new PlainVisitor();
+         tmp = "**test*h";
+         nod = new Text();
+         nodeList = new ArrayList<Node>();
+         tokenList = new ArrayList<Token>();
         p.tokenize(tmp,nod);
         nodeList = p.getNode();
         tokenList = nod.getTokenList();
         assertEquals("test",tokenList.get(2).getContent());
-    }
-    
-    @Test
-    public void testEmphasis_5() {
-        PlainVisitor p = new PlainVisitor();
-        String tmp = "hi___hi";
-        Text nod = new Text();
-        ArrayList<Node> nodeList = new ArrayList<Node>();
-        ArrayList<Token> tokenList = new ArrayList<Token>();
+         p = new PlainVisitor();
+         tmp = "hi___hi";
+         nod = new Text();
+         nodeList = new ArrayList<Node>();
+         tokenList = new ArrayList<Token>();
         p.tokenize(tmp,nod);
         nodeList = p.getNode();
         tokenList = nod.getTokenList();
         assertEquals("hi___hi",tokenList.get(0).getContent());
-    }
-    
-    
-    @Test
-    public void testEmphasis_6() {
-        PlainVisitor p = new PlainVisitor();
-        String tmp = "hi***hi";
-        Text nod = new Text();
-        ArrayList<Node> nodeList = new ArrayList<Node>();
-        ArrayList<Token> tokenList = new ArrayList<Token>();
+         p = new PlainVisitor();
+         tmp = "hi***hi";
+         nod = new Text();
+         nodeList = new ArrayList<Node>();
+         tokenList = new ArrayList<Token>();
         p.tokenize(tmp,nod);
         nodeList = p.getNode();
         tokenList = nod.getTokenList();
         assertEquals("hi***hi",tokenList.get(0).getContent());
-    }
-    
-    @Test
-    public void testEmphasis_7() {
-        PlainVisitor p = new PlainVisitor();
-        String tmp = "hi**hi";
-        Text nod = new Text();
-        ArrayList<Node> nodeList = new ArrayList<Node>();
-        ArrayList<Token> tokenList = new ArrayList<Token>();
+         p = new PlainVisitor();
+         tmp = "hi**hi";
+         nod = new Text();
+         nodeList = new ArrayList<Node>();
+         tokenList = new ArrayList<Token>();
         p.tokenize(tmp,nod);
         nodeList = p.getNode();
         tokenList = nod.getTokenList();
         assertEquals("hi",tokenList.get(0).getContent());
-    }
-    
-    @Test
-    public void testEmphasis_8() {
-        PlainVisitor p = new PlainVisitor();
-        String tmp = "hi__hi";
-        Text nod = new Text();
-        ArrayList<Node> nodeList = new ArrayList<Node>();
-        ArrayList<Token> tokenList = new ArrayList<Token>();
+         p = new PlainVisitor();
+         tmp = "hi__hi";
+         nod = new Text();
+         nodeList = new ArrayList<Node>();
+         tokenList = new ArrayList<Token>();
         p.tokenize(tmp,nod);
         nodeList = p.getNode();
         tokenList = nod.getTokenList();
@@ -136,42 +102,33 @@ public class PlainVisitorTest
         nodeList = p.getNode();
         tokenList = nod.getTokenList();
         assertEquals("test",tokenList.get(1).getContent());
-    }
-
-    @Test
-    public void testItalic2() {
-        PlainVisitor p = new PlainVisitor();
-        String tmp = "*test*";
-        Text nod = new Text();
-        ArrayList<Node> nodeList = new ArrayList<Node>();
-        ArrayList<Token> tokenList = new ArrayList<Token>();
+        p = new PlainVisitor();
+        tmp = "*test*";
+        nod = new Text();
+        nodeList = new ArrayList<Node>();
+        tokenList = new ArrayList<Token>();
         p.tokenize(tmp,nod);
         nodeList = p.getNode();
         tokenList = nod.getTokenList();
         assertEquals("test",tokenList.get(1).getContent());
-    }
-    @Test
-    public void testItalic3() {
-        PlainVisitor p = new PlainVisitor();
-        String tmp = "h_h_";
-        Text nod = new Text();
-        ArrayList<Node> nodeList = new ArrayList<Node>();
-        ArrayList<Token> tokenList = new ArrayList<Token>();
+        p = new PlainVisitor();
+        tmp = "h_h_";
+        nod = new Text();
+        nodeList = new ArrayList<Node>();
+        tokenList = new ArrayList<Token>();
+        p.tokenize(tmp,nod);
+        nodeList = p.getNode();
+        tokenList = nod.getTokenList();
+        assertEquals("h",tokenList.get(0).getContent());
+         p = new PlainVisitor();
+         tmp = "h*h*";
+         nod = new Text();
+         nodeList = new ArrayList<Node>();
+         tokenList = new ArrayList<Token>();
         p.tokenize(tmp,nod);
         nodeList = p.getNode();
         tokenList = nod.getTokenList();
         assertEquals("h",tokenList.get(0).getContent());
     }
-    @Test
-    public void testItalic4() {
-        PlainVisitor p = new PlainVisitor();
-        String tmp = "h*h*";
-        Text nod = new Text();
-        ArrayList<Node> nodeList = new ArrayList<Node>();
-        ArrayList<Token> tokenList = new ArrayList<Token>();
-        p.tokenize(tmp,nod);
-        nodeList = p.getNode();
-        tokenList = nod.getTokenList();
-        assertEquals("h",tokenList.get(0).getContent());
-    }
+
 }
